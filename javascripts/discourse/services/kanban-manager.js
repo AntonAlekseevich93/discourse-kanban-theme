76ae5686a7b09348aebd9c79a103544e8cf546d2
Белоногов Антон <62636706+AntonAlekseevich93@users.noreply.github.com>
2026-01-08 19:20:19 +0300

    Update kanban-manager.js
    rename tag

--- javascripts/discourse/services/kanban-manager.js
+++ javascripts/discourse/services/kanban-manager.js
@@ -101,7 +101,7 @@ export default class KanbanManager extends Service {
     }
 
     if (descriptor === "default") {
-        descriptor = "tags:planned,todo,doing,done";
+        descriptor = "tags:backlog,planned,doing,done";
     }
 
     return descriptor.split(":");
